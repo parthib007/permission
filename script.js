69a4a310242ed07467a72ec914c4f80dd384d634
parthib_007 <98155243+parthib007@users.noreply.github.com>
2023-12-29 00:00:42 +0530

    Update script.js
    added a resize function that will reduce the size of no button on successive clicks

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 function nextPage() {
     window.location.href = "yes.html";
   }
-
-  function moveButton() {
+  var initialWidth = document.getElementById("noButton").offsetWidth;
+  var initialHeight = document.getElementById("noButton").offsetHeight;
+  function move_resizeButton() {
     var x =
       Math.random() *
         (window.innerWidth -
@@ -15,4 +16,12 @@ function nextPage() {
       48;
     document.getElementById("noButton").style.left = `${x}px`;
     document.getElementById("noButton").style.top = `${y}px`;
+    initialWidth -= 5;
+      initialHeight -= 5;
+      document.getElementById("noButton").style.width = `${Math.max(initialWidth, 0)}px`;
+      document.getElementById("noButton").style.height = `${Math.max(initialHeight, 0)}px`;
+      if (initialWidth <= 0 || initialHeight <= 0) {
+        // Hide the button
+        document.getElementById("noButton").style.display = "none";
+      }
   }
